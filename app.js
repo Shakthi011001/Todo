fc82da0aa00ce857f6e40860a80bf503b5bb3fdf
Shakthi011001 <shakthi011001@gmail.com>
2021-07-17 00:47:19 +0530

    Commit 5

--- app.js
+++ app.js
@@ -127,8 +127,9 @@ else{
 app.get("/about", function(req,res){
 res.render("about");
 })
+const port = process.env.PORT || 3000;
 
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(port, function() {
   console.log("Server has started on port 3000");
 })
